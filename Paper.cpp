9fbf16bf8f8827f8fea3120d94b476b08ac0ed11
Michael Ibrahim <mike.a.ibrahim@gmail.com>
2021-07-14 21:43:39 -0700

    Initial commit

---
+++ Paper.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+
+bool canCut(int w, int h, int n, int currN) {
+	if (currN >= n) { return true; }
+	else if (w % 2 == 0) {
+		return canCut(w / 2, h, n, currN * 2);
+	} else if (h % 2 == 0) {
+		return canCut(w, h / 2, n, currN * 2);
+	}
+	return false;
+}
+
+int main() {
+	int a;
+	cin >> a;
+	int w, h, n;
+
+	for (int i = 0; i < a; i++) {
+		cin >> w >> h >> n;
+
+		cout << (canCut(w, h, n, 1) ? "YES" : "NO") << '\n';
+	}
+}
---
+++ QAQ.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+	string str;
+	cin >> str;
+	int len = str.length();
+
+	int t = 0;
+	for (int i = 0; i < len; i++) {
+		if (str[i] != 'A') continue;
+
+		int l = 0, r = 0;
+		for (int j = i; j >= 0; j--) {
+			if (str[j] == 'Q') l++;
+		}
+		for (int k = i; k < len; k++) {
+			if (str[k] == 'Q') r++;
+		}
+
+		t += l * r;
+	}
+
+	cout << t << '\n';
+}
